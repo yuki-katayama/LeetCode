55f03697a6880536375a2ee0d72ad40d0bdc1899
yuki-katayama <katayu810@gmail.com>
2021-06-07 11:55:26 +0900

    add makeMap function in 02_Maximum_Area_of_a_Piece_of_Cake_After_Horizontal_and_Vertical_Cuts.py

--- June_2021/02_Maximum_Area_of_a_Piece_of_Cake_After_Horizontal_and_Vertical_Cuts.py
+++ June_2021/02_Maximum_Area_of_a_Piece_of_Cake_After_Horizontal_and_Vertical_Cuts.py
@@ -8,29 +8,32 @@ class Solution:
             for i, value in enumerate(list_c):
                 list_c[i] = value + i
                 i += 1
+        def makeMap(h, w):
+            map = [[0 for i in range(w + len(verticalCuts))] for j in range(h + len(horizontalCuts))]
+            for height, width in product(range(len(map)), range(len(map[0]))):
+                for hori in horizontalCuts:
+                    if hori == height:
+                        map[height][width] = 1
+                for ver in verticalCuts:
+                    if ver == width and map[height][width] == 0:
+                        map[height][width] = 1
+                    elif ver == width and map[height][width] == 1:
+                        map[height][width] = 2
+            return map
+        def calc_area_rec(height, width):
+            if height < 0 or width  < 0 or height >= len(map) or width >= len(map[0]) or map[height][width] != 0:
+                return (0)
+            map[height][width] = 1
+            return 1 + calc_area_rec(height, width + 1) + calc_area_rec(height, width - 1) + calc_area_rec(height + 1, width) + calc_area_rec(height - 1, width)
         horizontalCuts = sorted(horizontalCuts)
         inc_cut_index(horizontalCuts)
         verticalCuts = sorted(verticalCuts)
         inc_cut_index(verticalCuts)
-        map = [[0 for i in range(w + len(verticalCuts))] for j in range(h + len(horizontalCuts))]
-        for height, width in product(range(len(map)), range(len(map[0]))):
-            for hori in horizontalCuts:
-                if hori == height:
-                    map[height][width] = 1
-            for ver in verticalCuts:
-                if ver == width and map[height][width] == 0:
-                    map[height][width] = 1
-                elif ver == width and map[height][width] == 1:
-                    map[height][width] = 2
-        def rec(height, width):
-            if height < 0 or width  < 0 or height >= len(map) or width >= len(map[0]) or map[height][width] != 0:
-                return (0)
-            map[height][width] = 1
-            return 1 + rec(height, width + 1) + rec(height, width - 1) + rec(height + 1, width) + rec(height - 1, width)
+        map = makeMap(h, w)
         max = 0
         for height, width in product(range(len(map)), range(len(map[0]))):
             if (map[height][width] != 2):
-                tmp  = rec(height, width)
+                tmp  = calc_area_rec(height, width)
             if (max < tmp):
                 max = tmp
         return max
