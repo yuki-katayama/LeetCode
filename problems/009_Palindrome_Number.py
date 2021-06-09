7b202cba2de1ecec8f9bc23c64e9185dd8ac799a
yuki-katayama <katayu810@gmail.com>
2021-06-10 00:06:18 +0900

    009_Palindrome_Number.py

--- problems/009_Palindrome_Number.py
+++ problems/009_Palindrome_Number.py
@@ -1,12 +1,4 @@
-import math
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         x = str(x)
-        success = 1
-        for i in range(math.floor(len(x) / 2)):
-            if x[i] != x[-(i + 1)]:
-                success = 0
-                break
-        return (success == 1)
-solution = Solution()
-print(solution.isPalindrome(1122332211))
+        return (x == x[::-1])
